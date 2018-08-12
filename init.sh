0ac9b2de462323fc2cf508129a660ce2a1213843
Alex Yursha <alexyursha@gmail.com>
2018-08-12 01:09:42 -0700

    Add mounting drives to init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,16 @@ echo "-------------------"
 chmod 666 /sys/class/backlight/intel_backlight/brightness
 chmod 666 /sys/class/backlight/intel_backlight/max_brightness
 
+echo "-------------------"
+echo "Mounting LFS partition"
+echo "-------------------"
+mount /dev/sda2 /mnt/lfs
+
+echo "-------------------"
+echo "Mounting SD Card"
+echo "-------------------"
+mount /dev/sdb1 /mnt/lfs/sources
+
 DIR=/home/alex/Repos/scripts
 
 $DIR/connect-wifi.sh
